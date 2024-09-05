3f1d7d1fce5c2211cc40034dbaf232a98305014b
AdityaRaj212 <aditya.education212@gmail.com>
2024-09-06 00:56:09 +0530

    minor change in db path

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-// const url = "mongodb+srv://adityaeducation212:adityadb212@cluster0.jm0dl8u.mongodb.net/message-app?retryWrites=true&w=majority&appName=Cluster0";
-const url = "mongodb://127.0.0.1:27017/message_app";
+const url = "mongodb+srv://adityaeducation212:adityadb212@cluster0.jm0dl8u.mongodb.net/message-app?retryWrites=true&w=majority&appName=Cluster0";
+// const url = "mongodb://127.0.0.1:27017/message_app";
 // const url = process.env.MONGO_URI;
 
 export const connectUsingMongoose = async () => {
